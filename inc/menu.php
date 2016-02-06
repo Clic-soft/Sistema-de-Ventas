b094ed2d13dce00cfb9e6c4f045e7b89fff9abad
Andrés Domínguez <andres_gomez@live.com.mx>
2016-02-06 00:02:02 -0700

    .

--- inc/menu.php
+++ inc/menu.php
@@ -5,24 +5,21 @@
 					<li id="productos"  role="presentation" style="margin-top:20px;">
 						<a  href="<?php echo BASE_URL; ?>productos">
 							<i class="glyphicon glyphicon-bed"></i> Productos 
-							<span class="badge alert-primary pull-right">
-								<i class="glyphicon glyphicon-bell"></i></span>
+							
 						</a>
 					</li>
 
 					<li id="clientes"  role="presentation">
 						<a  href="<?php echo BASE_URL; ?>clientes">
 							<i class="glyphicon glyphicon-folder-open"></i> Clientes 
-							<span class="badge alert-primary pull-right">
-								<i class="glyphicon glyphicon-bell"></i></span>
+							
 						</a>
 					</li>
 
 					<li id="ventas"  role="presentation">
 						<a href="<?php echo BASE_URL; ?>ventas">
 							<i class="glyphicon glyphicon-credit-card"></i> Ventas 
-							<span class="badge alert-primary pull-right">
-								<i class="glyphicon glyphicon-bell"></i></span>
+							
 						</a>
 					</li>
 					
@@ -30,48 +27,42 @@
 					<li id="usuarios"  role="presentation">
 						<a  href="<?php echo BASE_URL; ?>usuarios">
 							<i class="glyphicon glyphicon-user"></i> Usuarios 
-							<span class="badge alert-primary pull-right">
-								<i class="glyphicon glyphicon-bell"></i></span>
+							
 						</a>
 					</li>
 
 					<li id="empleados"  role="presentation">
 						<a  href="<?php echo BASE_URL; ?>empleados">
 							<i class="glyphicon glyphicon-user"></i> Empleados 
-							<span class="badge alert-primary pull-right">
-								<i class="glyphicon glyphicon-bell"></i></span>
+							
 						</a>
 					</li>
 
 					<li id="vehiculos"  role="presentation">
 						<a  href="<?php echo BASE_URL; ?>vehiculos">
 							<i class="glyphicon glyphicon-user"></i> Vehiculos 
-							<span class="badge alert-primary pull-right">
-								<i class="glyphicon glyphicon-bell"></i></span>
+							
 						</a>
 					</li>
 
 					<li id="proveedores" class="disable" role="presentation">
 						<a  onClick="javascript:alert('La opción está deshabilitada por el momento. \nSi tiene alguna duda, contacte a su desarrollador.')">
 							<i class="glyphicon glyphicon-scale"></i> Proveedores 
-							<span class="badge alert-primary pull-right">
-								<i class="glyphicon glyphicon-bell"></i></span>
+							
 						</a>
 					</li>
 
 					<li id="compras" class="disable"  role="presentation">
 						<a  onClick="javascript:alert('La opción está deshabilitada por el momento. \nSi tiene alguna duda, contacte a su desarrollador.')" >
 							<i class="glyphicon glyphicon-shopping-cart"></i> Insumos 
-							<span class="badge alert-primary pull-right">
-								<i class="glyphicon glyphicon-bell"></i></span>
+							
 						</a>
 					</li>
 
 					<li id="reportes" class="disable" role="presentation">
 						<a onClick="javascript:alert('La opción está deshabilitada por el momento. \nSi tiene alguna duda, contacte a su desarrollador.')">
 							<i class="glyphicon glyphicon-list-alt"></i> Reportes 
-							<span class="badge alert-primary pull-right">
-								<i class="glyphicon glyphicon-bell"></i></span>
+							
 						</a>
 					</li>
 				</ul>
