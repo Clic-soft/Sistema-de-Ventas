e54a0ae8fa34e00886ed75eae84df73b20b33b91
jorinzo <j_andres272@hotmail.com>
2016-03-30 16:38:08 -0500

    reportes nuevos y ajustes

--- controllers/reportesController.php
+++ controllers/reportesController.php
@@ -20,6 +20,82 @@ class reportesController extends Controller {
     }
 
 
+    //////////////////////// REPORTE CLIENTES //////////////////////////
+
+    public function buscar_cliente() {
+
+            $this->_view->titulo = 'Sistema De Ventas';
+            $this->_view->clientes = $this->_reportes->getclientes();           
+            $this->_view->renderizar('buscar_cliente', 'reportes');
+
+    }
+
+    public function lista_clientes() {
+
+            $this->_view->titulo = 'Sistema De Ventas';
+
+            if ($this->getInt('guardar') == 1) {
+                
+            $this->_view->datos = (object) $_POST; /* No se debe realizar de esta formaaaa */
+            //Se valida que las dos contraseñas digitadas coincidan
+
+            $_SESSION["id_cliente"]=$this->getInt('id_cliente');
+
+            $_SESSION["fecha_ini"]=$this->getSql('fecha_ini');
+            $_SESSION["fecha_fin"]=$this->getSql('fecha_fin');
+
+
+            $id_cliente = $_SESSION["id_cliente"];
+            $condicion = "";
+
+
+            if ($id_cliente > 0) {
+                $condicion= $condicion . " AND c.id = ".$id_cliente;
+            }
+
+            if ($_SESSION["fecha_ini"] != "" && $_SESSION["fecha_fin"] != "") {
+                $condicion= $condicion . " AND c.fecha_creacion between  '".$_SESSION["fecha_ini"] ."' AND '" .$_SESSION["fecha_fin"]."'";
+            }
+
+            
+
+            }
+            $this->_view->reporte = $this->_reportes->getClienterep($condicion);
+            $this->_view->renderizar('listado_clientes', 'reportes');
+
+    }
+
+    public function exportar_listado_clientes() {
+
+            $this->_view->titulo = 'Sistema De Ventas';
+                
+            $this->_view->datos = (object) $_POST; /* No se debe realizar de esta formaaaa */
+            //Se valida que las dos contraseñas digitadas coincidan
+
+            if ($_SESSION["fecha_ini"]!="" && $_SESSION["fecha_fin"]!="") {
+                $fecha_ini = date('Y-m-d', strtotime($_SESSION["fecha_ini"]));
+                $fecha_fin = date('Y-m-d', strtotime($_SESSION["fecha_fin"]));
+            }
+
+            $id_cliente = $_SESSION["id_cliente"];
+            
+            $condicion = "";
+
+
+            if ($id_cliente > 0) {
+                $condicion= $condicion . " AND c.id = ".$id_cliente;
+            }
+
+            if ($_SESSION["fecha_ini"] != "" && $_SESSION["fecha_fin"] != "") {
+                $condicion= $condicion . " AND e.fecha_creacion between  '".$fecha_ini ."' AND '" .$_SESSION["fecha_fin"]."'";
+            }
+
+            $this->_view->reporte = $this->_reportes->getClienterep($condicion);
+            $this->_view->renderizar('exportar_listado_clientes',false,true);
+
+    }
+
+
 
     //////////////////////// REPORTE FACTURAS //////////////////////////
 
@@ -93,6 +169,82 @@ class reportesController extends Controller {
 
             $this->_view->reporte = $this->_reportes->getFacturas($condicion);
             $this->_view->renderizar('exportar_listado_facturas',false,true);
+
+    }
+
+
+        //////////////////////// REPORTE FACTURAS //////////////////////////
+
+    public function buscar_certificado() {
+
+            $this->_view->titulo = 'Sistema De Ventas';
+            $this->_view->clientes = $this->_reportes->getclientes();           
+            $this->_view->renderizar('buscar_certificado', 'reportes');
+
+    }
+
+    public function lista_certificado() {
+
+            $this->_view->titulo = 'Sistema De Ventas';
+
+            if ($this->getInt('guardar') == 1) {
+                
+            $this->_view->datos = (object) $_POST; /* No se debe realizar de esta formaaaa */
+            //Se valida que las dos contraseñas digitadas coincidan
+
+            $_SESSION["id_cliente"]=$this->getInt('id_cliente');
+
+            $_SESSION["fecha_ini"]=$this->getSql('fecha_ini');
+            $_SESSION["fecha_fin"]=$this->getSql('fecha_fin');
+
+
+            $id_cliente = $_SESSION["id_cliente"];
+            $condicion = "";
+
+
+            if ($id_cliente > 0) {
+                $condicion= $condicion . " AND c.id = ".$id_cliente;
+            }
+
+            if ($_SESSION["fecha_ini"] != "" && $_SESSION["fecha_fin"] != "") {
+                $condicion= $condicion . " AND e.fecha_venta between  '".$_SESSION["fecha_ini"] ."' AND '" .$_SESSION["fecha_fin"]."'";
+            }
+
+            
+
+            }
+            $this->_view->reporte = $this->_reportes->getFacturas($condicion);
+            $this->_view->renderizar('listado_certificados', 'reportes');
+
+    }
+
+    public function exportar_listado_certificado() {
+
+            $this->_view->titulo = 'Sistema De Ventas';
+                
+            $this->_view->datos = (object) $_POST; /* No se debe realizar de esta formaaaa */
+            //Se valida que las dos contraseñas digitadas coincidan
+
+            if ($_SESSION["fecha_ini"]!="" && $_SESSION["fecha_fin"]!="") {
+                $fecha_ini = date('Y-m-d', strtotime($_SESSION["fecha_ini"]));
+                $fecha_fin = date('Y-m-d', strtotime($_SESSION["fecha_fin"]));
+            }
+
+            $id_cliente = $_SESSION["id_cliente"];
+            
+            $condicion = "";
+
+
+            if ($id_cliente > 0) {
+                $condicion= $condicion . " AND c.id = ".$id_cliente;
+            }
+
+            if ($_SESSION["fecha_ini"] != "" && $_SESSION["fecha_fin"] != "") {
+                $condicion= $condicion . " AND e.fecha_venta between  '".$fecha_ini ."' AND '" .$_SESSION["fecha_fin"]."'";
+            }
+
+            $this->_view->reporte = $this->_reportes->getFacturas($condicion);
+            $this->_view->renderizar('exportar_listado_certificados',false,true);
 
     }
 
@@ -172,6 +324,82 @@ class reportesController extends Controller {
 
             $this->_view->reporte = $this->_reportes->getCompras($condicion);
             $this->_view->renderizar('exportar_listado_compras',false,true);
+
+    }
+
+
+    //////////////////////// REPORTE General //////////////////////////
+
+
+
+    public function buscar_general() {
+
+            $this->_view->titulo = 'Sistema De Ventas';
+            $this->_view->clientes = $this->_reportes->getclientes();           
+            $this->_view->renderizar('buscar_general', 'reportes');
+
+    }
+
+    public function lista_general() {
+
+            $this->_view->titulo = 'Sistema De Ventas';
+
+            if ($this->getInt('guardar') == 1) {
+                
+            $this->_view->datos = (object) $_POST; /* No se debe realizar de esta formaaaa */
+            //Se valida que las dos contraseñas digitadas coincidan
+
+            $_SESSION["id_cliente"]=$this->getInt('id_cliente');
+
+            $_SESSION["fecha_ini"]=$this->getSql('fecha_ini');
+            $_SESSION["fecha_fin"]=$this->getSql('fecha_fin');
+
+
+            $id_cliente = $_SESSION["id_cliente"];
+            $condicion = "";
+
+
+            if ($id_cliente > 0) {
+                $condicion= $condicion . " AND c.id = ".$id_cliente;
+            }
+
+            if ($_SESSION["fecha_ini"] != "" && $_SESSION["fecha_fin"] != "") {
+                $condicion= $condicion . " AND e.fecha_venta between  '".$_SESSION["fecha_ini"] ."' AND '" .$_SESSION["fecha_fin"]."'";
+            }
+
+            }
+            $this->_view->reporte = $this->_reportes->getGeneral($condicion);
+            $this->_view->renderizar('listado_general', 'reportes');
+
+    }
+
+    public function exportar_listado_general() {
+
+            $this->_view->titulo = 'Sistema De Ventas';
+                
+            $this->_view->datos = (object) $_POST; /* No se debe realizar de esta formaaaa */
+            //Se valida que las dos contraseñas digitadas coincidan
+
+            if ($_SESSION["fecha_ini"]!="" && $_SESSION["fecha_fin"]!="") {
+                $fecha_ini = date('Y-m-d', strtotime($_SESSION["fecha_ini"]));
+                $fecha_fin = date('Y-m-d', strtotime($_SESSION["fecha_fin"]));
+            }
+
+            $id_cliente = $_SESSION["id_cliente"];
+            
+            $condicion = "";
+
+
+            if ($id_cliente > 0) {
+                $condicion= $condicion . " AND c.id = ".$id_cliente;
+            }
+
+            if ($_SESSION["fecha_ini"] != "" && $_SESSION["fecha_fin"] != "") {
+                $condicion= $condicion . " AND e.fecha_venta between  '".$fecha_ini ."' AND '" .$_SESSION["fecha_fin"]."'";
+            }
+
+            $this->_view->reporte = $this->_reportes->getGeneral($condicion);
+            $this->_view->renderizar('exportar_listado_general',false,true);
 
     }
 
--- models/reportesModel.php
+++ models/reportesModel.php
@@ -27,6 +27,21 @@ class reportesModel extends Model {
         return $consulta;
     }
 
+    public function getClienterep($condicion = "") {
+        //Se crea y ejecuta la consulta
+            $consulta = $this->_db->get_results("SELECT c.*,td.tipo_documento as tipdoc, tc.tipo_cliente, ciu.ciudad, d.departamento
+                                                FROM clientes as c,tipo_clientes as tc, tipo_documento as td, ciudades as ciu,
+                                                departamentos as d
+                                                WHERE c.id_tipo_cliente=tc.id
+                                                AND c.tipo_documento=td.id
+                                                AND c.id_depto=d.id
+                                                AND c.id_ciudad=ciu.id
+                                                 ".$condicion. "
+                                                 order by c.razon_social DESC;");
+        //Se retorna la consulta y se recorren los registros
+        return $consulta;
+    }
+
     public function getCompras($condicion = "") {
         //Se crea y ejecuta la consulta
             $consulta = $this->_db->get_results("SELECT e.*,p.*
@@ -41,6 +56,31 @@ class reportesModel extends Model {
         //Se crea y ejecuta la consulta
             $consulta = $this->_db->get_results("SELECT * FROM proveedores");
         //Se retorna la consulta y se recorren los registros
+        return $consulta;
+    }
+
+    public function getGeneral($condicion = "") {
+        //Se crea y ejecuta la consulta
+            $consulta = $this->_db->get_results("SELECT e.*,c.nomcom, em.nombres, em.apellidos, v.placa,
+                                                des.nombres as desnom, des.apellidos as desape,
+                                                tc.tipo_cliente, td.tipo_documento as tdoc, c.nit,
+                                                ciu.ciudad, d.departamento
+                                                FROM encabezado_venta as e, clientes as c, empleados as em, 
+                                                vehiculos as v, empleados as des, tipo_clientes as tc, 
+                                                tipo_documento as td, ciudades as ciu, departamentos as d
+                                                WHERE e.id_cliente=c.id 
+                                                AND e.id_empleado=em.id 
+                                                AND e.id_placa=v.id
+                                                and e.estado_venta = 4
+                                                and e.id_despachador = des.id
+                                                and c.id_tipo_cliente = tc.id
+                                                and c.tipo_documento =td.id
+                                                and c.id_ciudad=ciu.id 
+                                                and c.id_depto= d.id 
+                                                ".$condicion. " 
+                                                order by e.estado_venta ASC, e.fecha_venta DESC;");
+
+             //Se retorna la consulta y se recorren los registros
         return $consulta;
     }
 }
